25166ceba316a17374c2a16611fc846f531dc6b5
Jedrzej Kotkowski <jedrzejk143@gmail.com>
2023-07-11 22:30:40 +0200

    syntax

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,32 +1,32 @@
--- examples for your init.lua
-
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  renderer = {
-      group_empty = true,
-  },
-  view = {
-      width = 45,
-      relativenumber = true,
-  },
-  filters = {
-      dotfiles = false,
-  },
-})
-
---always open the tree
-local function open_nvim_tree()
-
-  -- open the tree
-  require("nvim-tree.api").tree.open()
-end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+---- examples for your init.lua
+--
+---- disable netrw at the very start of your init.lua (strongly advised)
+--vim.g.loaded_netrw = 1
+--vim.g.loaded_netrwPlugin = 1
+--
+---- empty setup using defaults
+--require("nvim-tree").setup()
+--
+---- OR setup with some options
+--require("nvim-tree").setup({
+--  sort_by = "case_sensitive",
+--  renderer = {
+--      group_empty = true,
+--  },
+--  view = {
+--      width = 45,
+--      relativenumber = true,
+--  },
+--  filters = {
+--      dotfiles = false,
+--  },
+--})
+--
+----always open the tree
+--local function open_nvim_tree()
+--
+--  -- open the tree
+--  require("nvim-tree.api").tree.open()
+--end
+--
+--vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
